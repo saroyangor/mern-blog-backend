4e84145cc64f700b0ca3a4a60d556d9632920284
saroyangor <dr.saroyan98@gmail.com>
2022-07-18 18:07:10 +0400

    process

--- index.js
+++ index.js
@@ -3,11 +3,16 @@ import mongoose from "mongoose"
 import multer from "multer"
 import cors from "cors"
 
-import {registerValidator, loginValidator, postCreateValidator} from "./validators/index.js"
-import {UserController, PostController} from "./controllers/index.js"
-import {checkAuth, handleValidationErrors} from "./utils/index.js"
+import {
+    registerValidator,
+    loginValidator,
+    postCreateValidator,
+} from "./validators/index.js"
+import { UserController, PostController } from "./controllers/index.js"
+import { checkAuth, handleValidationErrors } from "./utils/index.js"
 
-mongoose.connect("mongodb+srv://admin:admin@cluster0.p3mty.mongodb.net/blog?retryWrites=true&w=majority")
+mongoose
+    .connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("DB connected")
     })
@@ -21,22 +26,32 @@ const storage = multer.diskStorage({
     },
     filename: (_, file, cb) => {
         cb(null, file.originalname)
-    }
+    },
 })
 
-const upload = multer({storage})
+const upload = multer({ storage })
 
 app.use(express.json())
 app.use(cors())
 app.use("/uploads", express.static("uploads"))
 
-app.post("/auth/login", loginValidator, handleValidationErrors, UserController.login)
-app.post("/auth/register", registerValidator, handleValidationErrors, UserController.register)
+app.post(
+    "/auth/login",
+    loginValidator,
+    handleValidationErrors,
+    UserController.login
+)
+app.post(
+    "/auth/register",
+    registerValidator,
+    handleValidationErrors,
+    UserController.register
+)
 app.get("/auth/me", checkAuth, UserController.getMe)
 
 app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
     res.json({
-        url: `/uploads/${req.file.originalname}`
+        url: `/uploads/${req.file.originalname}`,
     })
 })
 
@@ -45,13 +60,25 @@ app.get("/users", UserController.getUsers)
 
 app.get("/posts", PostController.getAll)
 app.get("/posts/:id", PostController.getOne)
-app.post("/posts", checkAuth, postCreateValidator, handleValidationErrors, PostController.create)
+app.post(
+    "/posts",
+    checkAuth,
+    postCreateValidator,
+    handleValidationErrors,
+    PostController.create
+)
 app.delete("/posts/:id", checkAuth, PostController.remove)
-app.patch("/posts/:id", checkAuth, postCreateValidator, handleValidationErrors, PostController.update)
+app.patch(
+    "/posts/:id",
+    checkAuth,
+    postCreateValidator,
+    handleValidationErrors,
+    PostController.update
+)
 
-app.listen(4444, (err) => {
+app.listen(process.env.PORT || 4444, (err) => {
     if (err) {
         return console.log(err)
     }
-    console.log("Server run 4444")
+    console.log("Server run")
 })
